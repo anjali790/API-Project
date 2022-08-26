951c29ec93736cc28802a7441c60dac0e7e3828e
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-26 16:03:12 +0530

    project

--- Covid-Update/app.js
+++ Covid-Update/app.js
@@ -2,21 +2,18 @@ const btn = document.querySelector('.btn');
 const display = document.querySelector('.display');
 const tableData = document.querySelector('.tableData');
 
-
 btn.addEventListener('click',(e)=>{
     e.preventDefault()
     fetch(`https://api.covid19api.com/summary`)
-    // fetch(`https://api.covid19api.com//total/dayone/country/"+input+"`)
-
-
+    
     .then(res=>res.json())
     .then(data=>{
         console.log(data)
 
             let html = `
             <td class="data">${data['Global']['TotalConfirmed']}</td>
-            <td class="data">${data['Global']['TotalDeaths']}</td>
             <td class="data">${data['Global']['TotalRecovered']}</td>
+            <td class="data">${data['Global']['TotalDeaths']}</td>
             `
     
             tableData.innerHTML= html;
